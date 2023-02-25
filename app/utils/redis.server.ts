e8882a85c4ca1825d79b9341007efe3d8afafc70
Gabriel <yo@gabrielmendezc.com>
2023-02-25 16:44:49 +0100

    prod deployment

--- app/utils/redis.server.ts
+++ app/utils/redis.server.ts
@@ -1,8 +1,16 @@
 import Redis from "ioredis";
 
-const { REDIS_URL } = process.env;
-if (typeof REDIS_URL !== "string") {
-  throw new Error("REDIS_URL environment variable not set");
+const { REDIS_HOST, REDIS_PASSWORD } = process.env;
+if (typeof REDIS_HOST !== "string") {
+  throw new Error("REDIS_HOST environment variable not set");
 }
 
-export const redis = new Redis(REDIS_URL);
+if (typeof REDIS_PASSWORD !== "string") {
+  throw new Error("REDIS_PASSWORD environment variable not set");
+}
+
+export const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  family: process.env.NODE_ENV === "production" ? 6 : undefined,
+  password: process.env.REDIS_PASSWORD,
+});
--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 // that way everything's referencing the same globals
 require("@remix-run/node/dist/globals").installGlobals();
 
-require("dotenv/config");
-
 if (process.env.NODE_ENV === "production") {
   require("./server-build");
 } else {
+  require("dotenv/config");
   require("./server");
 }
